222d8c97f3ec75424875628da9c48ecd85748675
trupal godhat <trupal12@gmail.com>
2025-03-04 01:00:02 +0530

    Implemented Routing , Responsive Navigationbar with Links and Initial page setup

---
+++ FrontEnd/src/component/Home/Home.jsx
@@ -0,0 +1,19 @@
+import styles from './Home.module.css'
+import '@fontsource/kadwa'
+import '@fontsource/jua'
+
+function Home() {
+
+  return (
+    <>
+      <div className='main_container bg-white'>
+
+        <h1>Home Page</h1>
+   
+      </div>
+    </>
+  )
+}
+
+
+export default Home
---
+++ FrontEnd/src/component/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import styles from './Profile.module.css'
+
+function Profile() {
+
+  return (
+    <>
+      <div className="main_container bg-white">
+
+        <h1> Profile Page</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default Profile
